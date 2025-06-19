bea5ec6209a2b81e9a332d0f592e4e31e0c38e1b
lukmanproject <lukmanoman17@gmail.com>
2025-06-19 18:14:42 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika lukman = new Matematika(9, 8);
+        
+        System.out.println("Hasil Penjumlahan : "+lukman.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+lukman.setPengurangan());
+        System.out.println("Hasil Perkalian   : "+lukman.setPerkalian());
+        System.out.println("Hasil Pembagian   : "+lukman.setPembagian());
+    }   
 }
